6816654a9ec853170cd92b73e32a180262aae0ab
zhangpanzhi <1083447590@qq.com>
2017-01-20 17:50:34 +0800

    是打发第三方

--- routes/index.js
+++ routes/index.js
@@ -1,60 +1,95 @@
 var express = require('express');
 var db = require('./common/db.js');
+var splitLimit = require('split-limit');
 var router = express.Router();
 /*
  * 首页
  * */
 router.get('/', function (req, res, next) {
-    var sql = "SELECT * FROM crawler_video  order by createTime  LIMIT 0,30 ";
+    var sql = "SELECT * FROM crawler_video WHERE parentType='娱乐' and type='搞笑'  order by createTime  LIMIT 0,30 ";
     db.query(sql, function (err, result) {
         if (err) {
             throw err;
         } else {
-            res.render('index', {vs: result});
+            res.render('index', {vs: result,pagesize:30,pageindex:1});
         }
     })
 });
 /*
+ * 首页异步加载视频数据页面
+ * */
+router.get('/page/:pagesize/:pageindex.html', function (req, res, next) {
+    var _pagesize=req.params.pagesize;
+    var _pageindex=req.params.pageindex;
+    var sql = "SELECT * FROM crawler_video  order by createTime ASC LIMIT "+(_pageindex-1)*_pagesize+","+_pagesize*_pageindex;
+    db.query(sql, function (err, result) {
+        if (err) {
+            throw err;
+        } else {
+            res.json( result);
+        }
+    });
+});
+/*
  * 根据类型加载不同类型的视频数据页面
  * */
-router.get('/:type:pagesize:pageindex.html', function (req, res, next) {
+router.get('/:pt/:type/:pagesize/:pageindex.html', function (req, res, next) {
     var _type=req.params.type;
     var _pagesize=req.params.pagesize;
     var _pageindex=req.params.pageindex;
-    var value=getTypeValueByKey(req.get(_type));
-    var vs=value.split('-');
+    var _value=getTypeValueByKey(_type);
+    var _pt=req.params.pt;
+    var vs=_value.split('-');
     var sql = "SELECT * FROM crawler_video WHERE parentType='"+vs[0]+"' and type='"+vs[1]+"'  order by createTime ASC LIMIT "+(_pageindex-1)*_pagesize+","+_pagesize*_pageindex;
     db.query(sql, function (err, result) {
         if (err) {
             throw err;
         } else {
-            res.render(""+vs[2]+"",{"parentType":vs[0],"childType":vs[1]});
+            res.render("video-list",{vs: result,p_active:_pt,c_active:_type,p_active_text:vs[0],c_active_text:vs[1],pagesize:_pagesize,pageindex:_pageindex});
         }
-    })
-
+    });
+});
+router.get('/page/:pt/:type/:pagesize/:pageindex.html', function (req, res, next) {
+    var _type=req.params.type;
+    var _pagesize=req.params.pagesize;
+    var _pageindex=req.params.pageindex;
+    var _value=getTypeValueByKey(_type);
+    var vs=_value.split('-');
+    var sql = "SELECT * FROM crawler_video WHERE parentType='"+vs[0]+"' and type='"+vs[1]+"'  order by createTime ASC LIMIT "+(_pageindex-1)*_pagesize+","+_pagesize*_pageindex;
+   console.log(sql)
+    db.query(sql, function (err, result) {
+        if (err) {
+            throw err;
+        } else {
+            res.json(result);
+        }
+    });
 });
 function getTypeValueByKey(key) {
     var map = {
-        "1": "娱乐-搞笑-entertainment",
-        "2": "娱乐-明星-star",
-        "3": "娱乐-美妆-beauty",
-        "4": "娱乐-男神-menGod",
-        "5": "娱乐-萌娃-adorableBaby",
-        "6": "娱乐-美食-deliciousFood ",
-        "7": "娱乐-音乐-music",
-        "8": "娱乐-舞蹈-dance",
-        "9": "娱乐-宠物-pet",
-        "10": "娱乐-吃秀-eatShow",
-        "11": "娱乐-手工-manual",
-        "12": "娱乐-影视-movies",
-        "13": "娱乐-生活-life",
-        "14": "娱乐-健身-bodybuilding",
-        "15": "娱乐-世界-world",
-        "16": "娱乐-新闻-news",
-        "17": "娱乐-体育-sports",
-        "18": "娱乐-军事-military",
-        "19": "娱乐-剧集-drama"
+        "0":'娱乐',
+        "1": "娱乐-搞笑",
+        "2": "娱乐-明星",
+        "3": "娱乐-女神",
+        "4": "娱乐-美妆",
+        "5": "娱乐-男神",
+        "6": "娱乐-萌娃",
+        "7": "娱乐-美食",
+        "8": "娱乐-音乐",
+        "9": "娱乐-舞蹈",
+        "10": "娱乐-宠物",
+        "11": "娱乐-吃秀",
+        "12": "娱乐-手工",
+        "13": "娱乐-影视",
+        "14": "娱乐-生活",
+        "15": "娱乐-健身",
+        "16": "娱乐-世界",
+        "17": "新闻-新闻",
+        "18": "体育-体育",
+        "19": "军事-军事",
+        "20": "剧集-剧集",
+        "21": "电影-电影"
     }
-    return map['1'];
+    return map[''+key+''];
 }
 module.exports = router;
